fc0ab8080bfa6c9ccd707f5c31127db0ed70dd20
Dotunj <dotunjolaosho@gmail.com>
2017-06-06 16:29:49 +0100

    initial commit

---
+++ hello.php
@@ -0,0 +1,20 @@
+<?php
+
+Echo "Hello World"; 
+
+?>
+
+<?php
+
+$server = "localhost";
+$username = "root";
+$password="";
+$db_name ="pabackend";
+
+//Establish connection
+$connect = mysqli_connect($server, $username, $password, $db_name);
+if(!$connect){
+	die("Connection failed: " .mysqli_connect_error());
+} else {
+	echo "Connected to Database successfully";
+}
